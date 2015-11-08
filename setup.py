efbcec638651fd50e92ee56672b07fbf545b0274
Nikita Kitaev <nikitakit@gmail.com>
2015-11-07 21:50:14 -0800

    Switch to GridFS for storing intermediate parameter values
    The previous approach of storing binary data directly in MongoDB
    ran up against MongoDB's size cap.

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['jupyter', 'ipyparallel', 'pymongo'],
+    install_requires=['jupyter', 'ipyparallel >=4.0.2', 'pymongo >=3.1'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
